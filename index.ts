88dae588cabcfef7d5c58b40165a95037f57691d
jahangir yousuf <jahangiryousuf09@gmail.com>
2024-05-08 16:20:15 +0500

    addind a cli simple calculator

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{name:"num1",
+type:"number",
+massage:"enter first number"},{
+  name:"num2",
+  type:"number",
+  message:"enter second number"
+  },{
+    massage:"select one operator to perform action",
+    type:"list",
+    name:"operator",
+    choices:["add","subtract","multiply","divide"],
+  } 
+])
+
+if (answer.operator=== "add"){console.log("your answer is,",answer.num1+answer.num2);
+} else if (answer.operator === "subtract"){console.log("your answer is",answer.num1-answer.num2);
+} else if(answer.operator === "multiply"){console.log("your answer is",answer.num1*answer.num2);
+} else if (answer.operator === "divide"){console.log("your answer is",answer.num1/answer.num2);
+}
+ else ("invalid operator")
+
